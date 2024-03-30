0441ba478eaf24a3d5aa95e2fcd81a2cb5ec6b45
wangyzh <wangyzh@umich.edu>
2024-03-29 21:12:48 -0400

    "add a test to homomixnet"

--- applications/mixnet_homo/graph_generator.py
+++ applications/mixnet_homo/graph_generator.py
@@ -1,29 +1,27 @@
 import numpy as np
 import random
 
-def check_probability_sum2one(normal_nodes, compromised_nodes, open_nodes):
-    pass
-
 
 class HOMOGraph():
     def __init__(self,
-                 num_layers,
-                 num_nodes_per_layer,
-                 false_alarm,
-                 false_negative,
-                 a_attack_cost,
-                 a_deploy_cost,
-                 a_maintain_cost,
-                 a_alpha,
-                 active_rate,
-                 d_exclude_cost,
-                 d_deploy_cost,
-                 d_maintain_cost,
-                 usage_threshold,
-                 d_penalty,
-                 d_beta,
-                 normal_nodes=None,
-                 compromised_nodes=None):
+                 num_layers, # Number of layers
+                 num_nodes_per_layer, # list of Number of nodes per layer
+                 false_alarm, # List of false alarm rate
+                 false_negative, # List of false negative rate
+                 a_attack_cost, # List of attacker's attacking cost
+                 a_deploy_cost, # List of attacker's deployment cost
+                 a_maintain_cost, # List of attacker's maintaining cost
+                 active_rate, # List of the possibility of sucessfully activate a node
+                 d_exclude_cost, # List of defender's cost on excluding a node
+                 d_deploy_cost, # List of defender's deployment cost
+                 d_maintain_cost, # List of defender's maintaining cost
+                 usage_threshold=0.03, # the lower bound of usage without penalty
+                 d_penalty=-50, # defender's penalty for insufficient usage
+                 a_alpha=500,  # coefficient for the reward
+                 d_beta=1000, # coefficient for the penalty of attacks.
+                 normal_nodes=None, # list of ratio of normal nodes
+                 compromised_nodes=None,
+                 seed=None): # list of ratio of compromised nodes
 
         self.num_layers = num_layers
         self.num_nodes_per_layer = num_nodes_per_layer
@@ -40,22 +38,31 @@ class HOMOGraph():
         self.usage_threshold = usage_threshold
         self.d_penalty = d_penalty
         self.d_beta = d_beta
+
+        if seed is not None:
+            np.random.seed(seed)
+            random.seed(seed)
+
         if normal_nodes is not None:
             self.normal_nodes = normal_nodes
         else:
-            self.normal_nodes = np.random.uniform(0, 1, num_layers)
+            # Uniformly sample the ratio of normal nodes.
+            self.normal_nodes = np.random.uniform(0.4, 1, num_layers)
 
         if compromised_nodes is not None:
             self.compromised_nodes = compromised_nodes
         else:
+            # Uniformly sample the ratio of comprehensive nodes.
             self.compromised_nodes = np.zeros(num_layers)
-            for i in range(num_layers):
-                self.compromised_nodes[i] = np.random.uniform(0, self.normal_nodes[i])
+            for layer in range(num_layers):
+                self.compromised_nodes[layer] = np.random.uniform(low=0, high=1-self.normal_nodes[layer])
 
         self.open_nodes = 1 - self.normal_nodes - self.compromised_nodes
 
+        # The ratio of nodes belongs to either players.
         self.att_deploy = np.zeros(self.num_layers)
         self.def_deploy = self.normal_nodes + self.compromised_nodes
+
 
     def step(self, def_actions, att_actions):
         """
@@ -71,51 +78,96 @@ class HOMOGraph():
         reward_att = 0
 
         for layer in range(self.num_layers):
+            print("----")
             # deployment
             num_open_nodes = int(self.open_nodes[layer] * self.num_nodes_per_layer[layer])
-            att_deploy = set(random.sample(range(num_open_nodes), num_open_nodes * att_actions[self.num_layers + layer]))
-            def_deploy = set(random.sample(range(num_open_nodes), num_open_nodes * def_actions[self.num_layers + layer]))
+            print("num_open_nodes:", num_open_nodes)
+            # Sample a set of nodes for each player and let defender's action override attacker's action.
+            att_deploy = set(random.sample(list(range(num_open_nodes)), int(num_open_nodes * att_actions[self.num_layers + layer])))
+            def_deploy = set(random.sample(list(range(num_open_nodes)), int(num_open_nodes * def_actions[self.num_layers + layer])))
+            print("att_deploy:", att_deploy)
+            print("def_deploy:", def_deploy)
 
             att_deploy_rate = len(att_deploy.difference(def_deploy)) / self.num_nodes_per_layer[layer]
             def_deploy_rate = len(def_deploy) / self.num_nodes_per_layer[layer]
 
+            print("att_deploy_rate", att_deploy_rate)
+            print("def_deploy_rate", def_deploy_rate)
+
             # attack
             num_normal_nodes = int(self.normal_nodes[layer] * self.num_nodes_per_layer[layer])
             num_active_nodes = int((self.normal_nodes[layer] + self.compromised_nodes[layer]) * self.num_nodes_per_layer[layer])
+            num_att_deploy_nodes = int(self.att_deploy[layer] * self.num_nodes_per_layer[layer])
+
+            print("3:", num_normal_nodes, num_active_nodes, num_att_deploy_nodes)
+
             att_compromised = set(
-                random.sample(range(num_active_nodes), num_normal_nodes * att_actions[layer] * self.active_rate[layer] + self.num_nodes_per_layer[layer] * self.compromised_nodes[layer]))
+                random.sample(list(range(num_active_nodes)), int(num_normal_nodes * att_actions[layer] * self.active_rate[layer] + self.num_nodes_per_layer[layer] * self.compromised_nodes[layer])))
             def_exclude = set(
-                random.sample(range(num_active_nodes), num_active_nodes * def_actions[layer]))
+                random.sample(list(range(num_active_nodes)), int(num_active_nodes * def_actions[layer])))
+            att_deploy = set(random.choices(list(att_compromised), k=num_att_deploy_nodes))
+
+            print("att_compromised:", att_compromised)
+            print("def_exclude:", def_exclude)
+            print("att_deploy:", att_deploy)
 
             att_compromised_rate = len(att_compromised.difference(def_exclude)) / self.num_nodes_per_layer[layer]
             def_exclude_rate = len(def_exclude) / self.num_nodes_per_layer[layer]
+            att_deploy_old_rate = len(att_deploy.difference(def_exclude)) / self.num_nodes_per_layer[layer]
+
+            print("att_compromised_rate:", att_compromised_rate)
+            print("def_exclude_rate:", def_exclude_rate)
+            print("att_deploy_old_rate:", att_deploy_old_rate)
 
             new_compromised_rate = att_compromised_rate + att_deploy_rate
             new_open_rate = self.open_nodes[layer] - att_deploy_rate - def_deploy_rate + def_exclude_rate
+            assert new_open_rate + new_compromised_rate <= 1
             new_normal_rate = 1 - new_open_rate - new_compromised_rate
 
+            # Update layer information
             self.compromised_nodes[layer] = new_compromised_rate
             self.normal_nodes[layer] = new_normal_rate
             self.open_nodes[layer] = new_open_rate
 
+            # Attacker's payoff
             reward_att += self.a_attack_cost[layer] * att_actions[layer] \
-                          + self.a_deploy_cost * att_actions[self.num_layers + layer] \
-                          + self.a_maintain_cost * self.att_deploy[layer]
-            self.att_deploy[layer] = att_deploy_rate
+                          + self.a_deploy_cost[layer] * att_actions[self.num_layers + layer] \
+                          + self.a_maintain_cost[layer] * self.att_deploy[layer]
+            self.att_deploy[layer] = att_deploy_old_rate + att_deploy_rate
+
+            # Defender's payoff
             reward_def += self.d_exclude_cost[layer] * def_actions[layer] \
-                          + self.d_deploy_cost * def_actions[self.num_layers + layer] \
-                          + self.d_maintain_cost * self.def_deploy[layer]
+                          + self.d_deploy_cost[layer] * def_actions[self.num_layers + layer] \
+                          + self.d_maintain_cost[layer] * self.def_deploy[layer]
             self.def_deploy[layer] = new_normal_rate + new_compromised_rate - self.att_deploy[layer]
 
+            print("REW att:", self.a_attack_cost[layer] * att_actions[layer],
+                  self.a_deploy_cost[layer] * att_actions[self.num_layers + layer],
+                  self.a_maintain_cost[layer] * self.att_deploy[layer])
+            print("REW def:", self.d_exclude_cost[layer] * def_actions[layer],
+                  self.d_deploy_cost[layer] * def_actions[self.num_layers + layer],
+                  self.d_maintain_cost[layer] * self.def_deploy[layer])
+
         reward_att += self.a_alpha * np.prod(self.compromised_nodes)
-        reward_def += -self.d_beta * np.prod(self.compromised_nodes)
+        reward_def -= self.d_beta * np.prod(self.compromised_nodes)
+
+        print("REW att2:", self.a_alpha * np.prod(self.compromised_nodes))
+        print("REW def2:", self.d_beta * np.prod(self.compromised_nodes))
 
         if np.prod(self.normal_nodes + self.compromised_nodes) < self.usage_threshold:
+            print("PENALTY")
             reward_def += self.d_penalty
+
+        # Update observations
+        self.update_graph_state()
+
+        return self.get_def_observation(), self.get_att_observation(), reward_def, reward_att, self.get_graph_state()
+
 
     def update_graph_state(self):
         """
         Get the state and players' obervations.
+        state is a list [ratio of normal nodes * num_layers ratio of compromised nodes * num_layers]
         """
         self.state = np.concatenate((self.normal_nodes, self.compromised_nodes), axis=None)
         self.def_obs_normal = []
--- applications/mixnet_homo/verify_graph_generator.py
+++ applications/mixnet_homo/verify_graph_generator.py
@@ -1,2 +1,57 @@
 import numpy as np
+import random
+
+from applications.mixnet_homo.graph_generator import HOMOGraph
+
+def run_graph():
+    graph = HOMOGraph(
+        num_layers=3,
+        num_nodes_per_layer=[1000, 1000, 1000],
+        false_alarm=[0.3, 0.2, 0.3], # false alarm rate
+        false_negative=[0.2, 0.2, 0.2], # false negative rate
+        a_attack_cost=[-400, -500, -400], # attacker's attacking cost
+        a_deploy_cost=[-200, -200, -200], # attacker's deployment cost
+        a_maintain_cost=[-50, -50, -50], # attacker's maintaining cost
+        active_rate=[0.3, 0.2, 0.3], # the possibility of sucessfully activate a node
+        d_exclude_cost=[-200, -200, -200], # defender's cost on excluding a node
+        d_deploy_cost=[-100, -100, -100], # defender's deployment cost
+        d_maintain_cost=[-30, -30, -30],
+        usage_threshold=0.03, # the lower bound of usage without penalty
+        d_penalty=-50, # defender's penalty for insufficient usage
+        a_alpha=50000,  # coefficient for the reward
+        d_beta=50000,
+        seed=100)
+
+    print("Normal:", graph.normal_nodes)
+    print("Compromised:", graph.compromised_nodes)
+    print("Open:", graph.open_nodes)
+
+    # print("====== Attack =======")
+    # def_actions = [0] * 6
+    # att_actions = [0.5, 0.5, 0.5, 0, 0, 0]
+    #
+    # obs_def, obs_att, reward_def, reward_att, state = graph.step(np.array(def_actions), np.array(att_actions))
+    # print("-----")
+    # print("state:", state)
+    # print("def obs:", obs_def)
+    # print("att obs:", obs_att)
+    # print("def rew:", reward_def)
+    # print("att rew:", reward_att)
+
+    print("====== Attack + deploy =======")
+    def_actions = [0.2, 0.2, 0.2, 0.8, 0.8, 0.8]
+    att_actions = [0.5, 0.5, 0.5, 0.2, 0.2, 0.2]
+
+    obs_def, obs_att, reward_def, reward_att, state = graph.step(np.array(def_actions), np.array(att_actions))
+    print("-----")
+    print("state:", state)
+    print("def obs:", obs_def)
+    print("att obs:", obs_att)
+    print("def rew:", reward_def)
+    print("att rew:", reward_att)
+
+
+if __name__ == "__main__":
+    run_graph()
+
 
